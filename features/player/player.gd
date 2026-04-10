4aa47acbfcaf605860e2f8afae336173925342fd
butbkadrug <butbkadrug@gmail.com>
2026-04-10 17:25:47 +0400

    added animation to the player

---
+++ features/player/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var _animation_player: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_animation_player.play("idle")
+	pass
