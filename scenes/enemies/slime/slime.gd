65ab078c468681976a2ed728a3b1617caf38a1f1
TheGreatAles <alesivanou@gmail.com>
2026-04-11 18:55:23 +0400

    slime changes

--- scenes/enemies/slime/slime.gd
+++ scenes/enemies/slime/slime.gd
@@ -10,8 +10,11 @@ var direction: Vector2
 @onready var _animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
+func _ready():
+	_animated_sprite_2d.play("vertical_walk")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	
 	direction = position.direction_to(target)
 	velocity = direction * speed * delta
@@ -21,3 +24,8 @@ func _process(delta: float) -> void:
 	if is_collision: 
 		if is_collision.get_collider().name == "Player":
 			speed = 0
+			hp = 0
+			
+func _process(delta: float) -> void:
+	if hp <= 0:
+		_animated_sprite_2d.play("death")
